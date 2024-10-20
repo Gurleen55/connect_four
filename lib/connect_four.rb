0ebb2214f9d3cddcd178d1b7f75718b9f10342d0
Gurleen <gurleensinghdalli@gmail.com>
2024-10-20 12:55:21 -0300

    add update_grid method and relevant tests

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -3,21 +3,51 @@ require_relative "player"
 
 # this will implement connect four game
 class ConnectFour
-  def initialize(player1, player2)
+  attr_accessor :board
+
+  def initialize(player1, player2, board = Grid.new)
     @player1 = player1
     @player2 = player2
-    @board = Grid.new
+    @board = board
+  end
+
+  def grid_initialzation
+    @board.make_grid
+    @board.assign_grid
   end
 
   def user_input
     puts "please choose a number between 1 - 10"
     choice = nil
+    # this loop will ensure the valid input is provided
     loop do
       choice = gets.chomp.to_i
       break if choice.between?(1, 10)
 
       puts "invalid choice, please choose a number between 1 - 10"
     end
+    # return the choice to used after
     choice
+  end
+
+  def turn(player)
+    puts player.name
+    loop do
+      input = user_input
+      break if update_grid?(input - 1, player)
+    end
+    @board.display_grid
+  end
+
+  def update_grid?(input, player)
+    # this will select the first column
+    column = board.grid[input]
+    (column.size - 1).downto(0).each do |index|
+      if column[index] == " "
+        column[index] = player.symbol
+        return true
+      end
+    end
+    puts "lane full, please try different lane"
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/connect_four"
 
 describe ConnectFour do
-  let(:player1) { double("Player") }
+  let(:player1) { double("Player", name: "Adam", symbol: "X") }
   let(:player2) { double("Player") }
   subject { described_class.new(player1, player2) }
   describe "#user_input" do
@@ -35,6 +35,88 @@ describe ConnectFour do
       it "prompts user to choose again until a valid choice is made" do
         expect(subject).to receive(:gets).twice
         subject.user_input
+      end
+    end
+  end
+
+  describe "#turn" do
+    context "when the grid isn't full and update_grid returns true" do
+      before do
+        allow(subject).to receive(:user_input).and_return(2)
+        allow(subject).to receive(:puts)
+        allow(subject).to receive(:update_grid?).and_return(true)
+      end
+      it "displays grid" do
+        expect(subject.instance_variable_get(:@board)).to receive(:display_grid)
+        subject.turn(player1)
+      end
+    end
+    context "when the grid is full and update_grid returns false" do
+      before do
+        allow(subject).to receive(:user_input).and_return(2)
+        allow(subject).to receive(:puts)
+        allow(subject).to receive(:update_grid?).and_return(false, true)
+        allow(subject.board).to receive(:display_grid)
+      end
+      it "calls user input method again" do
+        expect(subject).to receive(:user_input).twice
+        subject.turn(player1)
+      end
+    end
+  end
+
+  describe "#update_grid" do
+    let(:board) { subject.instance_variable_get(:@board) }
+    context "when user selects first column and there's no other symbols in the column" do
+      before do
+        allow(subject).to receive(:user_input).and_return(0)
+        board.grid = [Array.new(8, " ")]
+      end
+      it "updates the bottom cell of the grid with player symbol" do
+        grid_array = subject.board.grid
+        subject.update_grid?(subject.user_input, player1)
+        expect(grid_array[0][7]).to eql("X")
+      end
+    end
+    context "when user selects first column and the column is full" do
+      before do
+        allow(subject).to receive(:user_input).and_return(0)
+        allow(subject).to receive(:puts)
+        board.grid = [Array.new(8, "X")]
+      end
+      it "returns nil" do
+        expect(subject.update_grid?(subject.user_input, player1)).to be_nil
+      end
+    end
+    context "when user selects first column and there's no other symbols in the column" do
+      before do
+        allow(subject).to receive(:user_input).and_return(0)
+        board.grid = [Array.new(8, " ")]
+      end
+      it "returns true" do
+        expect(subject.update_grid?(subject.user_input, player1)).to be true
+      end
+    end
+    context "when user selects third column and there's no other symbols in the column" do
+      before do
+        allow(subject).to receive(:user_input).and_return(2)
+        board.grid = [Array.new(8, " "), Array.new(8, " "), Array.new(8, " ")]
+      end
+      it "updates the bottom cell of the grid with player symbol" do
+        grid_array = subject.board.grid
+        subject.update_grid?(subject.user_input, player1)
+        expect(grid_array[2][7]).to eql("X")
+      end
+    end
+    context "when user selects third column and the column has two symbols" do
+      before do
+        allow(subject).to receive(:user_input).and_return(2)
+        board.grid = [Array.new(8, " "), Array.new(8, " "), [" ", " ", " ", " ", " ", " ", "X", "X"]]
+      end
+      it "updates the bottom third element in the column" do
+        grid_array = subject.board.grid
+        subject.update_grid?(subject.user_input, player1)
+        expect(grid_array[2][5]).to eql("X")
       end
     end
   end
